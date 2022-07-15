d0a71de4f755c0f61f9422bb9a75310ceb79cdfe
Alex Javier Brito Muñoz <74514983+Alexprograming@users.noreply.github.com>
2022-07-15 16:17:55 -0500

    commit4

---
+++ file.js
@@ -0,0 +1,3 @@
+Modificaciones a archivo JS
+Agregamos linea de texto a archivo JS
+Cambio de rama en GIT
---
+++ file.sql
@@ -0,0 +1,3 @@
+Modificaciones a archivo SQL
+Agregamos linea de texto a archivo SQL
+Cambio de rama en GIT
